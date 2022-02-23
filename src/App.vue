16745a50220ab6886d1271a6cfd55f0f1d6410e8
Denis <48333782+dkanukov@users.noreply.github.com>
2022-02-23 14:30:31 +0300

    create footer and start header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
 <template>
-	<div>
-		Test
-	</div>
+  <div>
+    <HeaderDefault></HeaderDefault>
+    <FooterDefault></FooterDefault>
+  </div>
 </template>
+
+<script>
+import HeaderDefault from "@/components/HeaderDefault.vue";
+import FooterDefault from "@/components/FooterDefault.vue";
+export default {
+	components: {
+		HeaderDefault,
+		FooterDefault
+	},
+  data: () => {
+
+  }
+}
+</script>
