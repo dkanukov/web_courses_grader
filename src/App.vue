e2ced51b191dc0e886878e910254aacfabb53a5d
Denis <48333782+dkanukov@users.noreply.github.com>
2022-02-26 19:22:56 +0300

    add course card template, need to resolve trouble with photos path

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
 <template>
   <div>
 		<HeaderComp/>
+      <v-row class="mt-10">
+        <v-col
+          cols = "6"
+          class=""
+        >
+          <v-row>
+            <CourseCard/>
+            <CourseCard/>
+          </v-row>
+        </v-col>
+        <v-divider
+            vertical="true"
+        ></v-divider>
+        <v-col
+          cols = "6"
+          class=""
+        >
+        </v-col>
+      </v-row>
 		<FooterComp/>
   </div>
 </template>
 
 <script>
-import HeaderComp from './components/HeaderComp';
-import FooterComp from './components/FooterComp';
+import HeaderComp from '@/components/HeaderComp';
+import FooterComp from '@/components/FooterComp';
+import CourseCard from "@/components/CourseCard";
 
 export default {
 	components: {
 		HeaderComp,
-		FooterComp
+		FooterComp,
+    CourseCard
 	},
   data: () => {
 
