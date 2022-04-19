5762e4b80b0eaa20841d89758a3d703512465551
Denis <kanukov.denis@gmail.com>
2022-04-19 14:10:08 +0300

    statrt creating form for course creation

--- src/views/CreateCourseView.vue
+++ src/views/CreateCourseView.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
     <HeaderComp title="Создайте новый курс" link="/"/>
+    <v-container>
+
+      <v-row justify="space-between" class="mt-10">
+        <v-col>
+          <v-text-field
+              label="Введите имя"
+              :rules = "personRules"
+              hide-details="auto"
+          />
+        </v-col>
+        <v-col>
+          <v-text-field label="Введите фамилию"/>
+        </v-col>
+        <v-col>
+          <v-text-field label="Введите почту"/>
+        </v-col>
+      </v-row>
+
+    </v-container>
     <FooterComp/>
   </div>
 </template>
@@ -14,7 +33,13 @@ export default {
   components: {
     FooterComp,
     HeaderComp
-  }
+  },
+  data: () => ({
+    personRules: [
+      value => !!value || 'Required.',
+      value => (value && value.length >= 3) || 'Min 3 characters',
+    ],
+  }),
 }
 </script>
 
