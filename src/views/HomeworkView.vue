48e3c887435e877eaed0cbaef308493cb3fb0bd8
Denis <kanukov.denis@gmail.com>
2022-05-17 21:19:27 +0300

    add dragndrop to homeWorkView, need to resolve problem with save and deleting tasks

--- src/views/HomeworkView.vue
+++ src/views/HomeworkView.vue
@@ -9,38 +9,47 @@
               class="ml-10"
               label="Название домашнего задания"
               v-model="newHomeWork.homeworkName"
-              :rules="inputHomeworkRules"
-          >
+              :rules="inputHomeworkRules">
           </v-text-field>
 
           <v-file-input
               class="mt-15"
-              label="Тесты для проверки"
-
-          >
-
+              label="Тесты для проверки">
           </v-file-input>
 
           <v-file-input
               class="mt-5"
-              label="Дополнительные файлы задания"
-          >
-
+              label="Дополнительные файлы задания">
           </v-file-input>
 
           <v-textarea class="mt-5 ml-10"
                       label="Дополнительный текст домашнего задания"
-                      v-model="newHomeWork.homeWorkText"
-          />
+                      v-model="newHomeWork.homeWorkText"/>
+
           <v-row justify="space-around">
             <v-btn color="primary">Опубликовать</v-btn>
             <v-btn color="error">Отменить</v-btn>
           </v-row>
-
         </v-col>
 
+        <v-spacer/>
 
-
+        <v-col cols="5">
+          <draggable
+              class="mt-16"
+              :list="courseTasks.tasks"
+              item-key="element.id"
+              @start="dragging = true"
+              @end="dragging = false">
+              <template #item="{element}">
+                <div class="dragndrop_item mt-10" style="border-radius: 10px">
+                  <h3>{{element.name}}</h3>
+                </div>
+              </template>
+          </draggable>
+            <v-btn block="block">Сохранить изменения</v-btn>
+            <v-btn block="block">Отменить</v-btn>
+        </v-col>
 
       </v-row>
   </v-form>
@@ -51,13 +60,16 @@
 <script>
 import HeaderComp from "@/components/HeaderBackRowComp";
 import {getTasksByCourseId} from "@/services/fetchers";
+import draggable from "vuedraggable"
 export default {
   name: "AddHomework",
   components: {
-    HeaderComp
+    HeaderComp,
+    draggable
   },
   data() {
     return {
+      block: true,
       newHomeWork: {
         homeworkName: "",
         homeWorkText: ""
@@ -67,13 +79,13 @@ export default {
         value => !!value || 'Введите название',
         value => (value && value.length >= 3) || 'Название должно содержать больше 3 символов',
         value => (value && value.length >= 3) || 'Название должно содержать меньше 10 символов'
-      ]
+      ],
+      dragging: false,
     }
   },
   methods: {
     async getTasks() {
       this.courseTasks = await getTasksByCourseId(window.localStorage.getItem("recentlyVisitedCourse"));
-      console.log(this.courseTasks.tasks);
     }
   },
   mounted() {
@@ -83,7 +95,10 @@ export default {
 </script>
 
 <style scoped>
-.text_area {
-  margin-left: 200px;
+.dragndrop_item {
+  max-width: 300px;
+  text-align: center;
+  background-color: cornflowerblue;
 }
+
 </style>
